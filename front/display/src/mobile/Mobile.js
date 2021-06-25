33e2ec75454a4e146d54f74c605365305ea0725b
Alexis Besson <alexis.besson1@gmail.com>
2021-06-25 12:50:55 +0200

    work on mobile version

--- front/display/src/mobile/Mobile.js
+++ front/display/src/mobile/Mobile.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useSwipeable } from "react-swipeable";
 import axios from "axios";
+import moment from "moment";
 import shuffle from "../common/helpers/shuffleArray";
 import "./Mobile.css";
 
 import loading from "./assets/loading.svg";
 import me from "../common/assets/me.jpeg";
+import twitter from "../common/assets/twitter.svg";
 
 const FETCH_URL = "api/news";
 
@@ -28,7 +30,7 @@ function App() {
     });
     axios.get(FETCH_URL).then((res) => {
       if (res && res.status === 200 && res.data && res.data.length > 0) {
-        setItems(shuffle(res.data));
+        setItems(shuffle(res.data.filter(e => e.type === 'tweet')));
       }
     });
   }, []);
@@ -88,38 +90,91 @@ function App() {
     },
   });
 
+  function getFollowersCount(nb) {
+    const k = nb/1000;
+    if (k > 1000) {
+     const m = k / 1000;     
+     return `${m.toFixed(1)} M`
+    } else if (k > 1) {
+     return `${k.toFixed(1)} k`
+    } else {
+        return nb;
+    }
+  }
+
   function printItem(item) {
-    return (
-      <div className="item" {...swiped}>
-        <div
-          className="item-picture"
-          style={{ backgroundImage: 'url("' + item.image + '")' }}
-        >
-          <div style={{ flex: 2 }}></div>
-          <div className="item-title">
-            <a href={item.url} target="_blank" rel="noopener noreferrer">
-              {item.title}
-            </a>
-          </div>
-        </div>
-        <div className="item-content">
-          <div className="item-description">
-            {item.description && item.description.length > 250
-              ? item.description.substr(0, 250).concat("[...]")
-              : item.description}
-          </div>
-          <div className="item-meta">
-            <div className="item-date">{item.date || ""}</div>
-            <div className="item-author">
-              {item.source}
-              {item.author && item.author !== item.source
-                ? `, ${item.author}`
-                : ""}
+    if (item.type === 'RSS') {
+        return (
+          <div className="item" {...swiped}>
+            <div
+              className="item-picture"
+              style={{ backgroundImage: 'url("' + item.image + '")' }}
+            >
+              <div style={{ flex: 2 }}></div>
+              <div className="item-title">
+                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                  {item.title}
+                </a>
+              </div>
+            </div>
+            <div className="item-content">
+              <div className="item-description">
+                {item.description && item.description.length > 250
+                  ? item.description.substr(0, 250).concat("[...]")
+                  : item.description}
+              </div>
+              <div className="item-meta">
+                <div className="item-date">{item.date || ""}</div>
+                <div className="item-author">
+                  {item.source}
+                  {item.author && item.author !== item.source
+                    ? `, ${item.author}`
+                    : ""}
+                </div>
+              </div>
             </div>
           </div>
+        );
+    } else if (item.type === 'tweet') {
+      return (
+        <div
+          {...swiped}
+          className="item"
+        >
+          <div className="tweet-item-header">
+            <div className="tweet-item-header-left">
+                  <img
+                    src={item.user.profilePicture}
+                    className="tweet-item-profile-picture"
+                    alt="twitter profile"
+                  />
+              <div>
+                <span className="tweet-item-user-name">{item.user.name}</span>
+                <br />
+                <span className="tweet-item-user-screen-name">@{item.user.screenName}, { getFollowersCount(item.user.followers)} abonnés</span>
+              </div>
+            </div>
+          </div>
+          <div className="tweet-item-content">
+            <div className="tweet-item-text">
+              {item.text.replace(/https:\/\/t\.co\/[a-zA-Z0-9]*/, "")}
+            </div>
+            <div className="tweet-item-header-right">
+              <span>{ moment(item.createdAt).fromNow() }</span>
+              <img width="32" src={twitter} alt="twitter logo" />
+            </div>
+            {item.entities.media && item.entities.media.length > 0 && (
+              <div className="tweet-item-media" style={{backgroundImage: `url('${item.entities.media[0].media_url_https}')`}}>
+              </div>
+            )}
+            {!item.entities.media && item.user.profileBanner && (
+              <div className="tweet-item-media" style={{backgroundImage: `url('${item.user.profileBanner}')`}}>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 
   if (showCounters) {
